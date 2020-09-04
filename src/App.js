eab48b40fabda03e499debcad139f8d5e23389df
rahmat058 <md.kazi.rahmat@gmail.com>
2020-09-04 16:02:51 +0600

    React Simple todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,151 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { AiFillDelete, AiOutlineCheckSquare } from "react-icons/ai";
+
+import EditTask from "./components/EditTask";
+
+const todoList = [
+  {
+    name: "Finish UI Task",
+    isCompleted: false,
+  },
+  {
+    name: "Build Web App",
+    isCompleted: false,
+  },
+  {
+    name: "Learn Music",
+    isCompleted: false,
+  },
+  {
+    name: "Training Flutter",
+    isCompleted: false,
+  },
+];
 
 function App() {
+  const [taskName, setTaskName] = useState("");
+  const [searchTask, setSearchTask] = useState("");
+  const [taskList, setTaskList] = useState(todoList);
+
+  const addTask = () => {
+    if (taskName !== "") {
+      let taskData = {};
+      taskData.name = taskName;
+
+      setTaskList((oldData) => {
+        return [...oldData, taskData];
+      });
+    } else {
+      alert("Please insert a task name...")
+    }
+
+    setTaskName("");
+  };
+
+  const deleteTask = (index) => {
+    const res = taskList.filter((task, i) => {
+      if (i !== index) return task;
+    });
+
+    setTaskList(res);
+  };
+
+  useEffect(() => {
+    const res = taskList.filter((task) => {
+      return task.name.toLowerCase().indexOf(searchTask) !== -1; // returns true or false
+    });
+
+    if (searchTask !== "") {
+      setTaskList(res);
+    } else {
+      setTaskList(todoList);
+    }
+  }, [searchTask]);
+
+  const updateTask = (index, val) => {
+    const newTaskList = [...taskList];
+    newTaskList[index].name = val;
+    setTaskList(newTaskList);
+  };
+
+  const completeTask = (index) => {
+    const newTaskList = [...taskList];
+    newTaskList[index].isCompleted = true;
+    setTaskList(newTaskList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row to-do-row">
+        <div className="col-md-12">
+          <form>
+            <div className="form-row">
+              <div className="col-md-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Task name...."
+                  value={taskName}
+                  onChange={(e) => setTaskName(e.target.value)}
+                />
+              </div>
+              <div className="col-md-9">
+                <div className="d-flex justify-content-between">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={addTask}
+                  >
+                    Add Task
+                  </button>
+                  <input
+                    type="text"
+                    className="form-control mx-w-30"
+                    placeholder="Search task...."
+                    onChange={(e) => setSearchTask(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+
+          <div className="task-list">
+            <ul className="list-group">
+              {taskList.map((task, index) => (
+                <li
+                  key={index}
+                  className="list-group-item d-flex justify-content-between"
+                >
+                  <span className={task && task.isCompleted ? "completed-task" : ""}>
+                    {task && task.name}
+                  </span>{" "}
+                  <span className="d-flex">
+                    <EditTask
+                      task={task}
+                      updateTask={updateTask}
+                      index={index}
+                    />
+                    <span
+                      className="cursor-pointer"
+                      onClick={() => completeTask(index)}
+                      style={{ marginRight: 10 }}
+                    >
+                      <AiOutlineCheckSquare size={20} color="green" />
+                    </span>
+                    <span
+                      className="cursor-pointer"
+                      onClick={() => deleteTask(index)}
+                    >
+                      <AiFillDelete size={20} color="red" />
+                    </span>
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/EditTask.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import { AiOutlineEdit } from "react-icons/ai";
+
+const EditTask = ({ index, task, updateTask }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [updateTaskName, setUpdateTaskName] = useState('');
+ 
+  return (
+    <div>
+      <span
+        className="cursor-pointer"
+        style={{ marginRight: 10 }}
+        onClick={() => setShowModal(true)}
+      >
+        <AiOutlineEdit size={20} color="#4C7CFF" />{" "}
+      </span>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <label htmlFor="task">Task Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Task name...."
+            defaultValue={task && task.name}
+            onChange={(e) => setUpdateTaskName(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => {
+            updateTask(index, updateTaskName)
+            setShowModal(false)
+          }}>
+            Update
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default EditTask;
